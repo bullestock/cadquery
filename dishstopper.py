481a998f9580f9b03f6f76719344830eeb0545f3
Torsten Martinsen <torsten@bullestock.net>
2023-10-25 21:27:19 +0200

    Add flange.

--- dishstopper.py
+++ dishstopper.py
@@ -1,24 +1,26 @@
 import cadquery as cq
 import math
-from cq_server.ui import ui, show_object
 
-top_dia = 19.5
-bot_dia = 18
-h = 20
+top_dia = 18.5
+bot_dia = 17
+flange_dia = 20
+flange_h = 3
+h = 22
 spout_id = 5
 spout_od = 7
 spout_h = 15
 spout_offset = 4
 
-seal_frac = 0.25
+seal_frac = 0.5
 seal_dia = top_dia - (top_dia - bot_dia)*seal_frac - 1.0
+log(seal_dia)
 
 # create path
 path = (cq.Workplane("XY" )
         .circle(seal_dia/2)
         )
 
-seal_d = 2.5
+seal_d = 3.5
 seal_groove = (cq.Workplane("XZ")
                .transformed(offset=(seal_dia/2, (1-seal_frac)*h, 0))
                .circle(seal_d/2).sweep(path)
@@ -30,6 +32,9 @@ res = (cq.Workplane("XY")
        .workplane(offset=h)
        .circle(top_dia/2)
        .loft(combine=True)
+       .faces(">Z")
+       .circle(flange_dia/2)
+       .extrude(flange_h)
        .edges(">Z").fillet(1)
        .workplaneFromTagged("bot")
        .transformed(offset=(spout_offset, 0, 0))
@@ -45,4 +50,3 @@ res = (cq.Workplane("XY")
 res = res - seal_groove
 
 show_object(res)
-cq.exporters.export(res, 'dishstopper.stl')
